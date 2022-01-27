53578fa7c5ae29bd00e10279f3f83945d611ebda
Udeshya Dhungana <udeshyadhungana1@gmail.com>
2022-01-27 12:01:55 +0545

    top navbar prototype completed

--- src/components/commons/atomic/button.jsx
+++ src/components/commons/atomic/button.jsx
@@ -4,12 +4,13 @@ import { Button as ChakraButton } from "@chakra-ui/react";
 // TODO: needs to be adjusted for placing icons
 // TODO: loading state
 
-function Button({ children, shadow, filled }) {
+function Button({ children, shadow, filled, className }) {
     return (
         <ChakraButton
             colorScheme={'blue'}
             variant={filled? 'solid' : 'outline'}
             boxShadow={shadow? 'md' : 'inherit'}
+            className={className}
         >
             {children}
         </ChakraButton>
