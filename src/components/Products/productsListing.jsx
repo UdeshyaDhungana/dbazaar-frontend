66ca7726177d1ce6829f48bee6ea226776b80726
Udeshya Dhungana <udeshyadhungana1@gmail.com>
2022-01-30 12:17:34 +0545

    added categories navbar prototype

--- src/components/Products/productsListing.jsx
+++ src/components/Products/productsListing.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+// import { Select } from '@chakra-ui/react';
 
-function ProductsListing() {
+function ProductShowCase({ products }) {
     return ( 
-        <div>
-            List of Products
+        <div className=''>
+            {products.map(product => (
+                <div key={product.id}>
+                    {product.id}
+                    {product.name}
+                    {product.price}
+                    {product.description}
+                    {product.stars}
+                </div>
+            ))}
         </div>
      );
+}
+
+function ProductsListing({ products }) {
+    return (
+        <div>
+            {/* <Select placeholder='Sorty By'>
+                <option value='price'>Price</option>
+                <option value='a-z'>A-Z</option>
+                <option value='z-a'>Z-A</option>
+            </Select> */}
+            {/* Show all | Auction | Buy now */}
+            <ProductShowCase
+                products={products}
+            />
+        </div>
+    );
 }
 
 export default ProductsListing;
