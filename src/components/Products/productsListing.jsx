ccc45bc12e2320efd44741f6a6a086f9cb1b577b
Udeshya Dhungana <udeshyadhungana1@gmail.com>
2022-01-31 15:40:23 +0545

    used modal for product card click popup

--- src/components/Products/productsListing.jsx
+++ src/components/Products/productsListing.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import { Box, Image, Flex, Grid, Stack, Text, Heading, Icon } from '@chakra-ui/react';
+import {
+    Box,
+    Image,
+    Flex,
+    Grid,
+    Stack, 
+    Text, 
+    Heading, 
+    Icon,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    // ModalFooter, 
+    useDisclosure } from '@chakra-ui/react';
 import { Star } from 'phosphor-react';
+// import Button from '../commons/atomic/button';
 
 // Sample card from Airbnb
 
-const ProductCard = ({ product: { id, label, price, description, stars } }) => {
+const ProductCard = ({ onClick, product: { id, label, price, description, stars } }) => {
     return (
-        <Stack p={{ base: "0 2rem" }}>
+        <Stack
+            onClick={onClick}
+            _hover={{
+                boxShadow: "2xl",
+            }}
+            borderRadius={"xl"}
+            cursor={"pointer"}
+            p={{ base: "5px 2rem" }}>
             <Image borderRadius={'2xl'} objectFit="cover" src={'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
             } alt={'whatever'} />
             <Heading color="gray.600" size="md" textTransform="capitalize">
                 {label}
             </Heading>
-            <Text color="gray.600" isTruncated>
-                {description}
-            </Text>
             <Box>
                 <Box as="span" color="gray.600" fontSize="sm">
-                    Rs. &nbsp;
+                    Rs.&nbsp;
                 </Box>
                 {price}
             </Box>
@@ -25,10 +46,10 @@ const ProductCard = ({ product: { id, label, price, description, stars } }) => {
                 {Array(5)
                     .fill('')
                     .map((_, i) => (
-                        <Icon            
+                        <Icon
                             as={Star}
                             key={i}
-                            weight={i < stars ? 'fill': 'regular'}
+                            weight={i < stars ? 'fill' : 'regular'}
                             color={'orange.300'}
                         />
                     ))}
@@ -39,6 +60,7 @@ const ProductCard = ({ product: { id, label, price, description, stars } }) => {
 
 
 function ProductsListing({ products }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <Flex
             direction="row"
@@ -50,8 +72,28 @@ function ProductsListing({ products }) {
                 gridTemplateColumns="repeat( auto-fit, minmax(300px, 1fr) )"
             >
                 {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard
+                        onClick={onOpen}
+                        key={product.id}
+                        product={product} />
                 ))}
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Modal Title</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            Lorem Ipsum and shit
+                        </ModalBody>
+
+                        {/* <ModalFooter>
+                            <Button mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                            <Button>Secondary Action</Button>
+                        </ModalFooter> */}
+                    </ModalContent>
+                </Modal>
             </Grid>
         </Flex>
     );
